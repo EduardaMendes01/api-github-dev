486a2e1d797ca00f32fdd2982b235990fc782d1d
EduardaMendes01 <eduardamendes0100@gmail.com>
2024-07-01 23:41:59 -0300

    starting the main structure

--- src/pages/MainPage/RepositoriesPage/styles.js
+++ src/pages/MainPage/RepositoriesPage/styles.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const Container = styled.main `
+  display: flex;
+  min-height: 100vh;
+
+  @media screen and (max-width: ${(props) => props.theme.breakpoints.md}) {
+    
+  }
+`;
