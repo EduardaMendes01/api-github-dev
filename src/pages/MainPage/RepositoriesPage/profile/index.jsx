1d0cd29f1823a1ed3a434d1bc30a85da4fea24a0
EduardaMendes01 <eduardamendes0100@gmail.com>
2024-07-05 22:35:40 -0300

    add more personal informations

--- src/pages/MainPage/RepositoriesPage/profile/index.jsx
+++ src/pages/MainPage/RepositoriesPage/profile/index.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
+
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
 function Profile() {
@@ -11,15 +14,19 @@ function Profile() {
       </Header>
       <Inner>
         <Data>
-          30 followers 10 Following
+          <MdGroup size={20} />
+          30&nbsp;<i>followers</i>&nbsp;&middot;&nbsp;10&nbsp;<i>Following</i>
         </Data>
         <Data>
+          <MdWork size={20} />
           Company
         </Data>
         <Data>
+          <MdLocationCity size={20} />
           São Paulo
         </Data>
         <Data>
+          <MdLink />
           <a href='https://www.linkedin.com/in/eduarda-mendes01/'>Linkedin Profile</a>
         </Data>
       </Inner>
